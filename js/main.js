d61e86571a021293a104011455c1493cd13f05a0
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-07-25 10:31:36 -0300

    modificando el proyecto

--- js/main.js
+++ js/main.js
@@ -1,84 +1,100 @@
 alert( `
-Hola.
+        Hola.
 
-Para avanzar debe contestar todas las preguntas.`);
+        Para avanzar debe contestar todas las preguntas.`);
 
-const nombre = prompt( 'Introducir su Nombre:' );
-const apellido = prompt( 'Introducir su Apellido:' );
+let nombre = prompt( 'Introducir su Nombre:' );
 
-if (nombre != "" && apellido != "") 
-{
-alert( `
-Muy bien ${ nombre } ${ apellido }.
 
-Para avanzar ingrese su Edad.`);
-}
-else {
-      alert("el nombre y el apellido se requieren");
-      nombre = prompt( 'Introducir su Nombre:' );
-      apellido = prompt( 'Introducir su Apellido:' );
-    }
+        if (nombre != "") 
+        {
+          alert( ` Muy bien ${ nombre }.
+
+        Para avanzar ingrese su apellido.`);
+        }
+        else {
+          alert("el nombre se requieren");
+          nombre = prompt( 'Introducir su Nombre:' );
+        }
+ 
+let apellido = prompt( 'Introducir su Apellido:' );
+    
+              if (apellido != "") 
+              {
+              alert( ` Muy bien ${ apellido }.
+              
+              Para avanzar ingrese su Edad.`);
+              }
+              else {
+              alert("El apellido se requieren");
+              apellido = prompt( 'Introducir su Apellido:' );
+              }
 
 
 let edad =  parseInt(prompt("Ingrese su Edad"));
+              if (edad != "") {
+                alert("Debe Ingresar una edad")
+                edad = parseInt(prompt("Ingrese su Edad"));
+              }
+              while (edad > 1 && edad < 18) {
+              alert("Edad no Permitida");
+              edad = parseInt(prompt("Ingrese su Edad"));
 
-    while (edad > 1 && edad < 18) {
-      alert("Edad no Permitida");
-      edad = parseInt(prompt("Ingrese su Edad"));
-    }
+              }
 
 let pais = prompt("Ingrese su País de Nacimiento");
 
-switch (pais) {
-  case "argentina":
-    alert("su país de nacimiento es Argentina.");
-    break;
-
-  case "chile":
-    alert("su país de nacimiento es Chile.");
-    break;
-
-  case "colombia":
-    alert("su país de nacimiento es Colombia.");
-    break;
-
-    case "venezuela":
-        alert("su país de nacimiento es Venezuela.");
-        break;
-
-    case "Peru":
-      alert("su país de nacimiento es Peru.");
-        break;
-
-        case "brasil":
-            alert("su país de nacimiento es Brasil.");
+              switch (pais) {
+              case "argentina":
+                  alert("su país de nacimiento es Argentina.");
               break;
-  default:
-    alert("su país de nacimiento no esta en nuestra base de datos.");
-    break;
-};
 
-alert( `
-Genial.
+              case "chile":
+                alert("su país de nacimiento es Chile.");
+              break;
 
-Vamos avanzando muy bien, ahora te toca resolver las siguientes ecuaciones:`);
+              case "colombia":
+                alert("su país de nacimiento es Colombia.");
+              break;
+
+              case "venezuela":
+                alert("su país de nacimiento es Venezuela.");
+              break;
+
+              case "Peru":
+                alert("su país de nacimiento es Peru.");
+              break;
+
+              case "brasil":
+                alert("su país de nacimiento es Brasil.");
+              break;
+
+              default:
+                alert("su país de nacimiento no esta en nuestra base de datos.")
+                pais = prompt("Ingrese su País de Nacimiento");  
+             };
+
+alert( ` Genial.
+
+        Vamos avanzando muy bien, ahora te toca resolver las siguientes ecuaciones:`);
 
 let pregunta1 = prompt("La suma de dos numero Impares tiene como resultado un numero Par?, Responda Verdad o Falso");
 
-if (pregunta1 === "verdad" ){
-     alert("Excelente, vamos a la siguiente pregunta");
-} else {
-    alert("lo siento, vuelva a intentarlo");
-};
+              if (pregunta1 === "verdad" ){
+                  alert("Excelente, vamos a la siguiente pregunta");
+              } 
+              else {
+                  alert("lo siento, vuelva a intentarlo");
+                  pregunta1 = prompt("La suma de dos numero Impares tiene como resultado un numero Par?, Responda Verdad o Falso");
+              };
 
 
-alert( `
-    Excelente, te muestro tus datos ingresados:
+alert( `Excelente, te muestro tus datos ingresados:
 
-    Nombre: ${ nombre } ${ apellido }
-    Edad: ${ edad }
-    Nacionalidad: ${pais}
-    Respuesta correcta: ${ pregunta1 }` );
+        Nombre: ${ nombre } ${ apellido }
+        Edad: ${ edad }
+        Nacionalidad: ${pais}
+        Respuesta 1: ${ pregunta1 }` );
 
 
 
