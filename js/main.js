da11816b81ba69cacca7ea178d4e67b3cda90008
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-07-26 00:05:58 -0300

    agregando seccion nueva

--- js/main.js
+++ js/main.js
@@ -57,35 +57,35 @@ let país = prompt(`✏  Ingrese su País de Nacimiento 🇦🇵🚩...
                    ⚠️ Nuestra base de datos esta basada en países Sudamericanos`).toLowerCase();
 
         switch (país) {
-          case "Argentina":
+          case "argentina":
                 alert(`Su país de nacimiento es Argentina 🇷.`);
           break;
 
-          case "Chile":
+          case "chile":
                 alert(`Su país de nacimiento es Chile 🏴󠁵󠁳󠁴󠁸󠁿.`);
           break;
 
-          case "Colombia":
+          case "colombia":
                 alert(`Su país de nacimiento es Colombia 🇨🇴.`);
           break;
 
-          case "Venezuela":
+          case "venezuela":
                 alert(`Su país de nacimiento es Venezuela 🇻🇪.`);
           break;
 
-          case "Peru":
+          case "peru":
                 alert(`Su país de nacimiento es Peru 🇪.`);
           break;
 
-          case "Brasil":
+          case "brasil":
                 alert(`Su país de nacimiento es Brasil 🇧🇷.`);
           break;
 
-          case "Uruguay":
+          case "uruguay":
                 alert(`Su país de nacimiento es Uruguay 🇾🇺🇾.`);
           break;
               
-          case "Ecuador":
+          case "ecuador":
                 alert(`Su país de nacimiento es Ecuador.`);
           break;
 
@@ -102,7 +102,7 @@ alert( `🥳 Genial 👏.
                       
         🔜`);
 
-let pregunta1 = prompt("La suma de dos numero Impares tiene como resultado un numero Par?, Responda Verdadero o Falso");
+let pregunta1 = prompt("La suma de dos numero Impares tiene como resultado un numero Par?, Responda Verdadero o Falso").toLowerCase();
 
           if (pregunta1 === "verdadero" ){
                   alert(`Excelente 👏, vamos a la siguiente pregunta`);
@@ -162,10 +162,61 @@ alert( `👏Excelente👏, llegaste al final 🥇🥇🥇
              Nacionalidad: ${país} 
              Pregunta Respondida: ${pregunta1}
              Numero consultado ("Primo o No primo"): ${numeroIngresado}
-             El numero consultado Par o Impar fue: ${numeroIngresado1}`);
+             El numero consultado Par o Impar fue: ${numeroIngresado1}
+             
+             Ahora avanzamos al proximo nivel `);
+        alert (`En esta sección simularemos una tienda de compras, donde tu calcularías cuanto te quedaría el precio final`);
+    
+        alert (`ha entrado un producto único en su estilo, el Valor del producto es de $500 Ars ¿Está interesado?, de ser asi en la proxima ventana elija la cantidad de productos que desea agregar al carrito`);
+        
+        
+        let cantidadArticulo = parseInt(prompt(`¿Cuántos artículos deseas agregar`));{
+                  if (cantidadArticulo >= 1 && cantidadArticulo < 10);
+                  alert(`Ha agregado correctamente  ` + cantidadArticulo + ` artículos al carrito `);
+                } 
+
+        const suma = (a, b) => a + b;
+        const resta = (a, b) => a - b;
+        const iva = (x) => x * 0.21;
+        const multiplica = (a,b) => a * b;
+        const porcentajeDescuento = (x) => (((x*a)/100)-x);
+        
+        
+        let valorProducto = 500;
+        let descuento = 50;
+        
+        //CÁLCULO DEL PRECIO FINAL
+                  // valorProducto * iva = precioFinalProducto
+                  // precioFinalProducto * cantidadArticulo = montoCompra
+                  // montoCompra - porcentajeDescuento = totalApagar
+    let totalApagar = multiplica(suma(valorProducto,iva(valorProducto)),cantidadArticulo);{
+          alert("El Total Apagará $" + Math.round(totalApagar));
+        };
+
+          alert(`Hoy estamos recolectando un aporte especial para los niños de la calle, dime que porcentaje te gustaría donar en la siguiente ventana ingresa en números`);
+
+    
+    
+    function pedirDonacion(){
+      let donacion = prompt(`ingrese por favor el porcentaje a donar, el mismo se calcula en base a su total a pagar en la tienda`);
+                                
+              while (isNaN(donacion) || donacion.trim() === "" ) {
+                    donación = prompt("❌ Debe Ingresar un porcentaje Valido");
+                    }
+                    return parseFloat(donacion);
+                  };
+                    
+       let porcentaje = pedirDonacion();
+                    
+              if (porcentaje < 0) {
+                    alert(` ❌ Para continuar debe ingresar un porcentaje ⚠️`);
+                    porcentaje = parseInt(prompt(`❌ Debe Ingresar un porcentaje Valido`));
+                      }
+                    
+        alert("✅ Gracias! ha decidido donar el"  + ${porcentaje} +"% sobre el monto total de su factura 🔜");
+                
 
 
-
-
-
+    
+       
 
